a0a4109dc59d89d6b36e1123356577f1fd923566
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2018-07-11 21:26:23 +0300

    team component

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import flag from '../logo.svg';
 import './Team.css';
 
 class Team extends Component {
@@ -7,13 +6,16 @@ class Team extends Component {
   render() {
 
     return (
-      <div className="col-1">
+      <div className="col-2">
         <div className="team col-12">
             <span className="col-3">
-                <img className="countryFlag" src={flag} alt="country flag"/>
+                <img className="countryFlag" src={this.props.flag} alt="country flag"/>
             </span>
-            <span className="col-9 teamName">
+            <span className="col-6 teamName">
                 {this.props.team}
+            </span>
+            <span className="col-6 teamOverall">
+                {this.props.overall}
             </span>
         </div>
       </div>       
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Team from './components/Team';
 import registerServiceWorker from './registerServiceWorker';
+import brazilflag from './countries/br.svg';
 
-ReactDOM.render(<Team team="Brazil"/>, document.getElementById('root'));
+ReactDOM.render(<Team team="Brazil" flag={brazilflag} overall="89"/>, document.getElementById('root'));
 registerServiceWorker();
